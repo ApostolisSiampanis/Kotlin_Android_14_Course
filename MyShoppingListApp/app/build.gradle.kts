3016a4d0993a42dd55db28dde5438ff932e7fae7
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-01-25 14:04:50 +0200

    MyShoppingListApp using Google Maps, day 12

--- MyShoppingListApp/app/build.gradle.kts
+++ MyShoppingListApp/app/build.gradle.kts
@@ -51,6 +51,15 @@ android {
 
 dependencies {
 
+    implementation("com.google.maps.android:maps-compose:2.15.0")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
