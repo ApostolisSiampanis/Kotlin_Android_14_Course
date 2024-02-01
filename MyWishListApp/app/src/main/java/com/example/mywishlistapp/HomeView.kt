3210be157c61693df2f6f30196394259b281fbea
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-02-01 19:58:07 +0200

    MyWishListApp completed, day 13 completed

--- MyWishListApp/app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ MyWishListApp/app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,26 +1,82 @@
 package com.example.mywishlistapp
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.mywishlistapp.Data.DummyWish
+import com.example.mywishlistapp.Data.Wish
 
 @Composable
-fun HomeView() {
+fun HomeView(
+    navController: NavController,
+    viewModel: WishViewModel
+) {
     val context = LocalContext.current
     Scaffold(
         topBar = {AppBarView(title = "WishList", {
             Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
 
-        })}
-    ) {
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)){
+        })},
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = {
+                    Toast.makeText(context, "FAButton Clicked", Toast.LENGTH_LONG).show()
+                    navController.navigate(Screen.AddScreen.route)
 
+                }){
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+
+    ) {
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)){
+            items(DummyWish.wishList){
+                wish -> WishItem(wish = wish){
+
+            }
+            }
         }
     }
 
+}
+
+@Composable
+fun WishItem(wish: Wish, onClick: () -> Unit) {
+    Card(modifier = Modifier.fillMaxWidth()
+        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+        .clickable {
+            onClick()
+        },
+        elevation = 10.dp,
+        backgroundColor = Color.White
+    ) {
+        Column(modifier = Modifier.padding(16.dp)){
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
+        }
+    }
 }
---
+++ MyWishListApp/app/src/main/java/com/example/mywishlistapp/WishViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.mywishlistapp
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class WishViewModel: ViewModel() {
+
+    var wishTitleState by mutableStateOf("")
+    var wishDescriptionState by mutableStateOf("")
+
+    fun onWishTitleChanged(newString:String){
+        wishTitleState = newString
+    }
+
+    fun onWishDescriptionChanged(newString: String){
+        wishDescriptionState = newString
+    }
+}
